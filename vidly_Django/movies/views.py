531518db8810e666bdbb48e5dbe8538b5780de11
awaissaddiqui <awaissaddiqui143@gmail.com>
2023-09-06 12:31:32 +0500

    display and retrive data from database

--- vidly_Django/movies/views.py
+++ vidly_Django/movies/views.py
@@ -1,7 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Movie
 # Create your views here.
 
 def index(request):
-     return HttpResponse("Hello World !")
+     movies=Movie.objects.all()
+     output=' , '.join([m.title for m in movies])
+     """
+     #SELECT * FROM movies_movie
+     Movie.objects.filter(relase_year=2004)
+     #SELECT * FROM movies_movie WHERE ....
+     Movie.objects.get(id=1)
+     """
+
+     return HttpResponse(output)
