464222cd517f83f76ce52b6c7a8da1ad22adf0a1
awaissaddiqui <awaissaddiqui143@gmail.com>
2023-09-08 00:00:25 +0500

    adding bootstrap and costomize it

--- vidly_Django/movies/views.py
+++ vidly_Django/movies/views.py
@@ -5,12 +5,16 @@ from .models import Movie
 
 def index(request):
      movies=Movie.objects.all()
-     output=' , '.join([m.title for m in movies])
+     return render(request,'movies/index.html', {'movies': movies})
+
+def details(request, movie_id):
+     return HttpResponse(movie_id)
      """
+     output=' , '.join([m.title for m in movies])
      #SELECT * FROM movies_movie
      Movie.objects.filter(relase_year=2004)
      #SELECT * FROM movies_movie WHERE ....
      Movie.objects.get(id=1)
      """
 
-     return HttpResponse(output)
+     # return HttpResponse(output)
